d3b8870a3080b32ff306f11835fe83b18fe68fe3
MohRyan <ryanmoh735@gmail.com>
2024-12-21 19:41:08 +0700

    fix: CORS ( Error because Swagger )

--- server.ts
+++ server.ts
@@ -14,22 +14,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use("/uploads", express.static(path.join(__dirname, "src/uploads"))) # untuk menjadikan sebuah folder bisa diakses secara global
-app.use(
-  cors({
-    origin: ["http://localhost:4000", "https://server-tht-ptwin.vercel.app/"], // Sesuaikan origin
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-app.use('/swagger-ui', express.static(path.join(__dirname, 'swagger-ui')));
-app.use("/apiDocs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(cors());
 
 app.use(indexRouter);
 
 app.get("/", (req, res) => {
   res.send("THT-PTWIN");
-
 });
+
+app.use("/apiDocs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, async () => {
   console.log("🚀 ~ app.listen ~ PORT:", PORT);
